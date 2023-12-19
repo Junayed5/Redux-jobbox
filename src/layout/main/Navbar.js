4c159fda71588c63fc5f6cd28d0be7b01cd0e2a5
Junayed5 <mdjunayed352003@gmail.com>
2023-12-19 20:24:44 +0600

    updated

--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -8,7 +8,7 @@ import { logout } from "../../features/auth/authSlice";
 
 const Navbar = () => {
   const { pathname } = useLocation();
-  const { email } = useSelector((state) => state.auth);
+  const { email, role } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const handleSignOut = () => {
@@ -53,13 +53,23 @@ const Navbar = () => {
             </Link>
           </li>
         )}
-        {email && (
+        {email && role && (
           <li>
             <Link
               className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
               to="/dashboard"
             >
               Dashboard
+            </Link>
+          </li>
+        )}
+        {email && !role && (
+          <li>
+            <Link
+              className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+              to="/register"
+            >
+              Get Started
             </Link>
           </li>
         )}
